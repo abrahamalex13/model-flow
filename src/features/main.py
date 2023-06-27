5412268dd6a9cf39a0d8ae669fc3034dc9f42dd3
abrahamalex13 <alex.abraham13@gmail.com>
2023-06-26 20:39:19 -0500

    Add feature transform pipeline `main` call

---
+++ src/features/main.py
@@ -0,0 +1,47 @@
+import pickle
+from pathlib import Path
+import pandas as pd
+
+from src.config.core import config, transforms_args_adapters
+
+from src.data import extractors
+from src.data.db import engine
+from src.data.integrate_XY import integrate_XY
+
+from src.features.PipelineImputeScrubEnrichBasis import (
+    PipelineImputeScrubEnrichBasis,
+)
+
+
+query_x = extractors.QueryX(config.config_data)
+X = pd.read_sql_query(query_x.query, engine)
+
+query_y = extractors.QueryY(config.config_data)
+Y = pd.read_sql_query(query_y.query, engine)
+Y = Y.rename(columns={config.outcome_definition["title"]: "y"})
+
+XY = integrate_XY(X, Y, config.outcome_definition)
+
+X = XY[config.features]
+X_attributes = XY[config.dataset_attributes]
+y = XY["y"]
+
+if config.is_training_run:
+
+    pipeline = PipelineImputeScrubEnrichBasis(transforms_args_adapters)
+    pipeline.fit(X, y=y)
+    with open(config.outputs_path["feature_transforms_pipeline"], "wb") as f:
+        pickle.dump(pipeline, f)
+
+with open(config.outputs_path["feature_transforms_pipeline"], "rb") as f:
+    pipeline = pickle.load(f)
+
+X = pipeline.transform(X)
+
+X.to_csv(config.outputs_path["X"].with_suffix(".csv"), index=False)
+X.to_pickle(config.outputs_path["X"].with_suffix(".pkl"))
+X_attributes.to_csv(
+    Path(str(config.outputs_path["X"]) + "_attributes.csv"), index=False
+)
+
+y.to_csv(config.outputs_path["y"].with_suffix(".csv"), index=False)
