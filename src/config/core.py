4efcea48c9954c39d67e314caa35a9f6bfccd02e
abrahamalex13 <alex.abraham13@gmail.com>
2024-02-27 17:08:01 -0600

    Update `core` config paths

--- src/config/core.py
+++ src/config/core.py
@@ -3,12 +3,9 @@ from src.config.ConfigFeatureTransformsPipeline import (
     ConfigFeatureTransformsPipeline,
 )
 
-DIR = pathlib.Path(__file__).parent
-
 config = ConfigFeatureTransformsPipeline(
-    path_config_data=DIR / "data/config.yml",
-    path_config_features=DIR / "features/config.yml",
-    path_config_subdirs=DIR
-    / "utils/config_pipeline_outputs_subdirectories.yml",
+    path_config_data="src/data/config.yml",
+    path_config_features="src/features/config.yml",
+    path_config_subdirs="src/config/utils/config_pipeline_outputs_subdirectories.yml",
 )
 config.make_dirs()
