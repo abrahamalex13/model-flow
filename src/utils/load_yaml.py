0b5dc36c56f8fc40dfafac83773e952adcd58350
abrahamalex13 <alex.abraham13@gmail.com>
2024-02-26 21:22:17 -0600

    Remove load_yaml module

--- src/utils/load_yaml.py
+++
@@ -1,9 +0,0 @@
-from strictyaml import load
-
-def load_yaml_to_pydict(path_yaml):
-
-    with open(path_yaml, "r") as yml:
-        obj_strict = load(yml.read())
-        obj_dict = obj_strict.data
-
-    return obj_dict  
