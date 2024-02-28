2549e4b935ea144d387bfed607fd9e05f86e1191
abrahamalex13 <alex.abraham13@gmail.com>
2024-02-27 18:55:46 -0600

    Relocate composite config

--- src/core.py
+++ src/core.py
@@ -5,6 +5,6 @@ from src.config.ConfigFeatureTransformsPipeline import (
 config = ConfigFeatureTransformsPipeline(
     path_config_data="src/data/config.yml",
     path_config_features="src/features/config.yml",
-    path_config_subdirs="src/config/utils/config_pipeline_outputs_subdirectories.yml",
+    path_config_subdirs="src/utils/config_pipeline_outputs_subdirectories.yml",
 )
 config.make_dirs()
