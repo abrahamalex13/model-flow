cfe8ee6ad57064bc5b81c565226efb80407318bb
abrahamalex13 <alex.abraham13@gmail.com>
2024-02-27 19:51:32 -0600

    Add rationale for fillna in `integrate_XY`

--- src/data/integrate_XY.py
+++ src/data/integrate_XY.py
@@ -2,6 +2,12 @@ import pandas as pd
 
 
 def integrate_XY(X, Y, config_outcome_definition):
+    """
+    Separate step for X-Y integration accommodates reality that,
+    X and Y records not often stored together. Wait until integration step
+    to fill missing Y because, missing Y may not be apparent until 
+    alignment with X.
+    """
 
     XY = pd.merge(X, Y, how="left")
 
