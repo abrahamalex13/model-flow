dc9b150ce33c7fad61ff6960b4964a3617bc47d7
abrahamalex13 <alex.abraham13@gmail.com>
2024-03-06 20:54:24 -0600

    Continue prev

--- src/models/fit_evaluate/ridge.py
+++ src/models/fit_evaluate/ridge.py
@@ -4,29 +4,25 @@ import glmnet
 from sklearn import metrics
 import joblib
 
-from src.config.core import config
-from src.config.models import config_ridge
+from src.config import config
 
-PATH_MODEL_ULTIMATE = (
-    config.outputs_dir["pipeline_artifacts"] / "ridge.pkl"
-)
+PATH_MODEL_ULTIMATE = config.models_subdir / "ridge.pkl"
 PATH_FEATURE_IMPORTANCE_MODEL_ULTIMATE = (
-    config.outputs_dir["pipeline_artifacts"]
-    / "feature_importance_ridge.csv"
+    config.models_subdir / "feature_importance_ridge.csv"
 )
 
-X = pd.read_pickle(config.outputs_path["X_train"].with_suffix(".pkl"))
-y = pd.read_csv(config.outputs_path["y_train"].with_suffix(".csv"))["y"]
+X = pd.read_pickle(config.X_train_path.with_suffix(".pkl"))
+y = pd.read_csv(config.y_train_path.with_suffix(".csv"))["y"]
 
 
 # glmnet intelligently estimates _path_ of linear model solutions
 # path points are lambda values
 model = glmnet.ElasticNet(
     alpha=0, standardize=False, fit_intercept=True, n_splits=0, random_state=777
-    )
+)
 model = model.fit(X, y)
 
-tune_grid = pd.DataFrame({'lambda': model.lambda_path_})
+tune_grid = pd.DataFrame({"lambda": model.lambda_path_})
 
 
 if not config_ridge.model_lambda:
@@ -40,13 +36,13 @@ if not config_ridge.model_lambda:
 
     for i in range(tune_grid.shape[0]):
 
-        y_pred = model.predict(X_test, lamb=tune_grid['lambda'][i])
+        y_pred = model.predict(X_test, lamb=tune_grid["lambda"][i])
 
         # trial-specific score dict may store multiple metrics
         score = {}
 
         score["rmse"] = metrics.mean_squared_error(y_test, y_pred, squared=False)
-        score['mae'] = metrics.mean_absolute_error(y_test, y_pred)
+        score["mae"] = metrics.mean_absolute_error(y_test, y_pred)
         score["error_summary"] = score["rmse"]
 
         scores.append(score)
@@ -57,22 +53,20 @@ if not config_ridge.model_lambda:
     results = pd.concat([tune_grid, scores], axis=1)
 
     score_max = (
-        results['error_summary'].min() + 
-        multiplier_se_addto_min_error * results['error_summary'].std()
-        ) 
-    
-    idx_model_best = np.argwhere(results['error_summary'] <= score_max)[0][0]
+        results["error_summary"].min()
+        + multiplier_se_addto_min_error * results["error_summary"].std()
+    )
+
+    idx_model_best = np.argwhere(results["error_summary"] <= score_max)[0][0]
 
     print(results.iloc[idx_model_best])
-    
-elif config_ridge.model_lambda: 
 
-    idx_model_best = np.argwhere(
-        tune_grid['lambda'] <= config_ridge.model_lambda
-        )[0][0]
+elif config_ridge.model_lambda:
+
+    idx_model_best = np.argwhere(tune_grid["lambda"] <= config_ridge.model_lambda)[0][0]
 
 
-model.lambda_best_ = tune_grid['lambda'][idx_model_best]
+model.lambda_best_ = tune_grid["lambda"][idx_model_best]
 # coefficient path data structure:
 # one row is one feature
 # one column is one lambda value
@@ -80,13 +74,8 @@ model.coef_ = model.coef_path_[:, idx_model_best]
 model.intercept_ = model.intercept_path_[idx_model_best]
 joblib.dump(model, PATH_MODEL_ULTIMATE)
 
-feature_importance = (
-    pd.DataFrame({
-        "feature": X.columns, 
-        "score": np.abs(model.coef_),
-        "coef": model.coef_
-        })
-    .sort_values(["score"], ascending=False)
-    )
+feature_importance = pd.DataFrame(
+    {"feature": X.columns, "score": np.abs(model.coef_), "coef": model.coef_}
+).sort_values(["score"], ascending=False)
 
 feature_importance.to_csv(PATH_FEATURE_IMPORTANCE_MODEL_ULTIMATE, index=False)
