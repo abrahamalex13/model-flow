6205f67dcf1b55ac8769569d69ec8379472c7229
abrahamalex13 <alex.abraham13@gmail.com>
2024-03-06 20:30:48 -0600

    Update data element tested

--- tests/src/test_config_feature_transforms_pipeline.py
+++ tests/src/test_config_feature_transforms_pipeline.py
@@ -1,43 +1,25 @@
 from pathlib import Path
 
 
-def test_pipeline_outputs_subdir_levels(config_feature_transforms_pipeline):
+def test_data_processed_subdir(config_feature_transforms_pipeline):
 
-    expected = {
-        "data_processed": [
-            "war_annualized_geq1_within_3_years_since_draft",
-            "draftees_2013_thru_2017_train",
-            "complete_wide",
-        ],
-        "pipeline_artifacts": [
-            "war_annualized_geq1_within_3_years_since_draft",
-            "draftees_2013_thru_2017_train",
-            "complete_wide",
-        ],
-    }
+    expected = Path(
+        "./data/processed"
+        "/war_annualized_geq1_within_3_years_since_draft/"
+        "draftees_2013_thru_2017_train/"
+        "complete_wide/"
+    )
 
-    given = config_feature_transforms_pipeline.outputs_subdir_levels
-
-    assert given == expected
+    assert config_feature_transforms_pipeline.data_processed_subdir == expected
 
 
-def test_pipeline_outputs_dir(config_feature_transforms_pipeline):
+def test_models_subdir(config_feature_transforms_pipeline):
 
-    expected = {
-        "data_processed": Path(
-            "./data/processed"
-            "/war_annualized_geq1_within_3_years_since_draft/"
-            "draftees_2013_thru_2017_train/"
-            "complete_wide/"
-        ),
-        "pipeline_artifacts": Path(
-            "./models"
-            "/war_annualized_geq1_within_3_years_since_draft/"
-            "draftees_2013_thru_2017_train/"
-            "complete_wide/"
-        ),
-    }
+    expected = Path(
+        "./models"
+        "/war_annualized_geq1_within_3_years_since_draft/"
+        "draftees_2013_thru_2017_train/"
+        "complete_wide/"
+    )
 
-    given = config_feature_transforms_pipeline.outputs_dir
-
-    assert given == expected
+    assert config_feature_transforms_pipeline.models_subdir == expected
