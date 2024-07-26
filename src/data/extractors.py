439f538f76b3c5650c60f6c92010c1b9c25af77e
abrahamalex13 <alex.abraham13@gmail.com>
2024-07-26 07:45:35 -0500

    Drop assumed `is_valid` column

--- src/data/extractors.py
+++ src/data/extractors.py
@@ -33,11 +33,11 @@ class ExtractorX:
 
     def extract_database(self, details_source):
 
-        query = f"SELECT * FROM {details_source['location']} WHERE is_valid"
+        query = f"SELECT * FROM {details_source['location']}"
 
         if details_source["do_filter"]:
             query += (
-                f" AND {self.filter['field_min']} <= {self.filter['field']} "
+                f" WHERE {self.filter['field_min']} <= {self.filter['field']} "
                 f" AND {self.filter['field']} <= {self.filter['field_max']}"
             )
 
